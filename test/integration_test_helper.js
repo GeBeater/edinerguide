7c1a4270a143e6f34e736b7e2fb7ef50cc7b06d5
GeBeater <info@gebeat.com>
2013-11-25 10:37:56 +0100

    test fix and version update * add kind of mock for google maps api * fix integrations tests * change version from 0.0.1-alpha.10 to 0.0.1-alpha.11 [CI modifikation] --skip-ci

--- test/integration_test_helper.js
+++ test/integration_test_helper.js
@@ -3,19 +3,3 @@ document.write('<div id="ember-testing-container"><div id="ember-testing"></div>
 App.rootElement = '#ember-testing';
 App.setupForTesting();
 App.injectTestHelpers();
-
-
-function exists(selector) {
-    return !!find(selector).length;
-}
-
-function stubEndpointForHttpRequest(url, json) {
-    $.mockjax({
-        url: url,
-        dataType: 'json',
-        responseText: json
-    });
-}
-
-$.mockjaxSettings.logging = false;
-$.mockjaxSettings.responseTime = 0;
