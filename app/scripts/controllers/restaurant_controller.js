e6a515f3da1b8c8080b10b76e3ddcaa1927b8d61
GeBeater <info@gebeat.com>
2013-12-30 16:42:53 +0100

    restaurant investigation feature * use promise for restaurant fetching ajax calls * refactoring restaurant controller * add hide/display toggling for restaurant template * change version from 0.0.1-alpha.19 to 0.0.1-alpha.20 * add basic error handling functionality for restaurant api calls --skip-ci

--- app/scripts/controllers/restaurant_controller.js
+++ app/scripts/controllers/restaurant_controller.js
@@ -1,7 +1,8 @@
 App.RestaurantController = Ember.Controller.extend({
 
-    // TODO add awesome toggle widget with color if restaurant filled otherwise hide or grey
     isEmpty: true,
+
+    error: 'A restaurant could not be found.',
 
     id: null,
     name: null,
@@ -13,8 +14,8 @@ App.RestaurantController = Ember.Controller.extend({
     amplify: null,
 
     actions: {
-        receiveCoordinates: function(latlnt) {
-            this.findRestaurant(latlnt);
+        receiveCoordinates: function(latlng) {
+            this.findRestaurant(latlng);
         }
     },
     setRestaurant: function(restaurant) {
@@ -25,25 +26,76 @@ App.RestaurantController = Ember.Controller.extend({
         this.set('location.city', restaurant.location.city);
         this.set('location.country', restaurant.location.country);
         this.set('url', restaurant.url);
+        this.set('contact.phone', restaurant.contact.formattedPhone);
         this.set('category', restaurant.categories[0].name);
     },
     /**
-     * Fetch a random restaurant from foursquare API based on given latitude and longtitude.
+     * Call the foursquare API proxy to fetch some restaurants by given latlng.
      *
-     * TODO add error handling
+     * @param latlng
+     * @param amplifyProxy
+     * @param error
+     * @returns {Ember.RSVP.Promise}
+     */
+    fetchRestaurants: function(latlng, amplifyProxy, error) {
+        return new Ember.RSVP.Promise(function(resolve, reject){
+            var query = { "ll": latlng, "query": "restaurant", "radius": 800, "explore": 1 };
+            amplifyProxy.get('request')("foursquare", query, function(data) {
+                if((null === data) || (undefined === data.meta.code)
+                    || (200 !== data.meta.code) || (data.response.totalResults < 1)) {
+                    // reject
+                    reject(error);
+                } else {
+                    // succeed
+                    resolve(data);
+                }
+            });
+        });
+    },
+    /**
+     * Call the foursquare API proxy to fetch one restaurant specified by id.
+     *
+     * @param id
+     * @param amplifyProxy
+     * @param error
+     * @returns {Ember.RSVP.Promise}
+     */
+    fetchRestaurant: function(id, amplifyProxy, error) {
+        return new Ember.RSVP.Promise(function(resolve, reject){
+            amplifyProxy.get('request')("foursquare", { id: id }, function(data) {
+                if((null === data) || (undefined === data.meta.code) || (200 !== data.meta.code)) {
+                    // reject
+                    reject("blablabla");
+                } else {
+                    // succeed
+                    resolve(data);
+                }
+            });
+        });
+    },
+    /**
+     * // TODO display error message in user interface
+     *
+     * Fetch a random restaurant from foursquare API based on given latlng.
      *
      * @param latlng comma separated latitude and longitude
      */
     findRestaurant: function(latlng) {
-        var query = { "ll": latlng, "query": "restaurant", "radius": 500, "explore": 1 };
-        var controller = this;
-        var amplify = this.get('amplify');
-        this.get('amplify').get('request')( "foursquare", query, function(data) {
-            var restaurants = data.response.groups[0].items;
-            var restaurant = restaurants[Math.floor(Math.random() * restaurants.length)].venue;
-            amplify.get('request')( "foursquare", { id: restaurant.id }, function(data) {
-                controller.setRestaurant(data.response.venue);
-            });
+        var self = this;
+        var amplify = this.get('amplify')
+        var errorMsg = this.get('error');
+        this.fetchRestaurants(latlng, amplify, errorMsg).then(function(restaurantsJson) {
+                // use a random restaurant item from the first group
+                var restaurants = restaurantsJson.response.groups[0].items;
+                var restaurant = restaurants[Math.floor(Math.random() * restaurants.length)].venue;
+
+                return self.fetchRestaurant(restaurant.id, amplify, errorMsg);
+        }).then(function(restaurantJson) {
+                self.setRestaurant(restaurantJson.response.venue);
+                self.set('isEmpty', false);
+        }).fail(function(error) {
+                console.log(error);
+                self.set('isEmpty', true);
         });
     }
 });
--- app/scripts/proxies/amplify_objectproxy.js
+++ app/scripts/proxies/amplify_objectproxy.js
@@ -12,6 +12,5 @@ App.AmplifyObjectProxy = Ember.ObjectProxy.extend({
             dataType: "json",
             type: "GET"
         });
-
     }
 });
